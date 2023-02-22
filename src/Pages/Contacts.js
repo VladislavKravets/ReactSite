45b4aaa047179288010913e7213fa0d36dbe9b5f
Vladislav <vlad.kravets12@gmail.com>
2023-02-22 13:00:28 +0200

    add new pages (About,Blog,Contacts,Home), add tab,and Col, Nav, Row.

---
+++ src/Pages/Contacts.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Contacts extends Component {
+    render() {
+        return (
+            <div>
+                Contracts
+            </div>
+        );
+    }
+}
+
+export default Contacts;
---
+++ src/Pages/Home.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import CarouselComponent from "../Components/CarouselComponent";
+
+class Home extends Component {
+    render() {
+        return (
+            <>
+                Home
+                <CarouselComponent/>
+            </>
+        );
+    }
+}
+
+export default Home;
